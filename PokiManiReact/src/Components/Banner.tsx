da13801d01d47b40671c9b3b3a1847e0e0f298f5
George W <georgeawiseman@gmail.com>
2025-11-26 16:31:35 -0800

    mild banner change

--- PokiManiReact/src/Components/Banner.tsx
+++ PokiManiReact/src/Components/Banner.tsx
@@ -1,5 +1,10 @@
 import "./Banner.scss";
 
 export default function Banner() {
-    return <div className="banner">Banner</div>;
+    return (
+        <div className="banner">
+            <div className="banner__loose_cash">Loose Cash</div>
+            <div className="banner__month_selector">Month Selector</div>
+        </div>
+    );
 }
